23ceb161a8393e53d3e6dc148e72ae55ecc7c9c0
Simon Gilchrist <sgilchrist123@gmail.com>
2024-02-26 01:09:35 +0000

    Trivial change to test git integration

--- starter.py
+++ starter.py
@@ -6,3 +6,6 @@ index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do growing up?")
 print(response)
+
+# testing git 
+# Testing git
